ef46909c507da6cb26e2f770c52b36d52a23fc08
tim <timthegoodness@gmail.com>
2022-10-20 00:09:21 +0800

    create Seeder

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,14 +1,54 @@
-const Restaurant = require('../restaurants')// 載入 resturants model
-const db = require('../../config/mongoose.js')
-const restaurantsList = require("../../restaurant.json").results
-require('dotenv').config()
+const bcrypt = require('bcryptjs')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
-db.once('open', () => {
-  console.log('mongodb connected!')
-  Restaurant.create(restaurantsList)
-    .then(() => {
-      console.log("restaurantSeeder create successfully!")
-      db.close()
-    })
-    .catch(err => console.log(err))
+const Restaurant = require('../restaurants') // 載入 todo model
+const restaurantList = require('../../restaurant.json').results
+
+const User = require('../users')
+const userList = require('../../userList.json').users
+const db = require('../../config/mongoose')
+
+userList.map(SEED_USER => {
+  db.once('open', () => {
+    console.log('mongodb connected!')
+    console.log(SEED_USER.name, SEED_USER.email)
+
+    Promise.all([
+      bcrypt
+        .genSalt(10)
+        .then(salt => bcrypt.hash(SEED_USER.password, salt))
+        .then(hash => User.create({
+          name: SEED_USER.name,
+          email: SEED_USER.email,
+          password: hash
+        }))
+        .then(user => {
+          // 取得已建立完成的 user id (要做關聯資料表)
+          const userId = user._id
+          // 取得 user 種子資料編號要建立的餐廳數量
+          const restaurantId = SEED_USER.restaurantId
+
+          // 這邊設定 userRestaurants 接收 restaurant 的餐廳資訊
+          // 一定要加上 return 把 restaurant 回傳, 不然會無法建立 restaurant 資料
+          const userRestaurants = restaurantId.map(id => {
+            const restaurant = restaurantList[id]
+
+            // 因為 restaurant 使用 const
+            // 所以用以下方式將從資料表取得的 user id 賦值給 restaurant 的 userId
+            restaurant.userId = userId
+            return restaurant
+          })
+
+          // 建立 restaurant 資料
+          return Restaurant.create(userRestaurants)
+        })
+    ])
+      .then(() => {
+        console.log('done.')
+        // 關閉 DB 連線
+        process.exit()
+      })
+  })
 })
